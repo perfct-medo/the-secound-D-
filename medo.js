7f5a1c62e60197d715745c3e0679b32d9106502f
perfct-medo <99294164+perfct-medo@users.noreply.github.com>
2022-06-06 22:39:37 +0200

    add up btn

--- medo.js
+++ medo.js
@@ -26,3 +26,20 @@ shClass.forEach((element) => {
     };
 });
 console.log(shClass);
+let btn = document.querySelector("#up");
+
+window.onscroll = function() {
+    if (window.scrollY >= 600) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+};
+
+btn.onclick = function() {
+    window.scrollTo({
+        left: 0,
+        top: 0,
+        behavior: "smooth",
+    });
+};
